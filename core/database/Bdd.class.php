789338263ece8793bc8e03f178e0de040d30acc4
mozelle remy <timoz974@hotmail.fr>
2017-08-25 14:04:44 +0300

    ajout fonction add

--- core/database/Bdd.class.php
+++ core/database/Bdd.class.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace core\database;
+
 use \PDO;
 
 class Bdd {
@@ -10,18 +11,18 @@ class Bdd {
     public $dbname = 'cnam';
     public $user = 'root';
     public $password = '';
-    
-    public function connexion (){
 
-        if($this->pdo === NULL){
+    public function connexion() {
+        if ($this->pdo === NULL) {
             $pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-
+            $pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_WARNING);
             $this->pdo = $pdo;
             return $this->pdo;
         }
         return $this->pdo;
     }
-}
 
-?>
+    public function add() {
+        
+    }
+}
